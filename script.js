859f18e25bbd2b3ae09f2e7db52f8cb4640bc269
theanuragshukla <tachyonanurag@gmail.com>
2022-10-25 00:56:37 +0530

    added cursor hover effect

--- script.js
+++ script.js
@@ -3,27 +3,29 @@ const ctx = cnvs.getContext('2d')
 
 cnvs.width = window.innerWidth
 cnvs.height = window.innerHeight
-
-const pos = [[100, 100], [200, 200]]
-
+const cursorRad = 150
+const pos = []
+const specialPoints = []
 const points = []
 
 class Point {
-	constructor({position, id}){
+	constructor({id}){
+		const v = 3
+		const theta = Math.random()*2*Math.PI;
 		this.velocity = {
-		x:Math.random()*5,
-		y:Math.random()*5,
-	}
+			x:v*Math.cos(theta),
+			y:v*Math.sin(theta),
+		}
 		this.id=id
 	}
 	draw(){
 		ctx.beginPath()
-		ctx.fillStyle = "red"
-		ctx.arc(pos[this.id][0], pos[this.id][1], 5, 0 , 2*Math.PI)
+		ctx.fillStyle = "#0ff"
+		ctx.arc(pos[this.id][0], pos[this.id][1], 2, 0 , 2*Math.PI)
 		ctx.fill()
 	}
 	update(){
-if(pos[this.id][1]>=cnvs.height || pos[this.id][1]<=0){
+		if(pos[this.id][1]>=cnvs.height || pos[this.id][1]<=0){
 			this.velocity.y*=-1
 		}
 		if(pos[this.id][0] <=0 || pos[this.id][0]>=cnvs.width){
@@ -36,17 +38,22 @@ if(pos[this.id][1]>=cnvs.height || pos[this.id][1]<=0){
 	}
 }
 const combinations = (arr) =>{
+	const tempArr = [...specialPoints, ...arr]
 	const ret = []
-	for(let i = 0; i< arr.length-1; i++){
-		for(let j = i+1;j<arr.length;j++){
-			ret.push([arr[i], arr[j]])
+	for(let i = 0; i< 1; i++){
+		let count = 0;
+		for(let j = i+1;j<tempArr.length;j++){
+			if(Math.abs(tempArr[i][0]-tempArr[j][0])<cursorRad && Math.abs(tempArr[i][1]-tempArr[j][1])<cursorRad){
+				//if(count>=2){
+					//break
+				//}
+				ret.push([tempArr[i], tempArr[j]])
+				count++
+			}
 		}
 	}
 	return ret
 }
-
-const p1 = new Point({id:0})
-const p2 = new Point({id:1})
 
 const animate = () => {
 	ctx.fillStyle="#000"
@@ -54,36 +61,42 @@ const animate = () => {
 	points.map(point=>{
 		point.update()
 	})
-	ctx.strokeStyle="#0f0"
-	ctx.strokeWidth="2"
+	ctx.strokeStyle="rgba(0, 255, 255, 0.2)"
+	ctx.strokeWidth="1"
 	const lines = combinations(pos);
 	lines.map(line=>{
 		ctx.moveTo(...line[0])
 		ctx.lineTo(...line[1])
 		ctx.stroke()
-		ctx.closePath()
 	})
 
-	ctx.strokeStyle="rgba(0,0,0,0)"
 	window.requestAnimationFrame(animate)
+	//setTimeout(animate, 1000/40)
 }
 
 window.onload = () => {
-	for(let i = 0; i< pos.length; i++){
-		points.push(new Point({
-			id:i
-		}))
-	}	
+	for(let i = 0 ; i<300; i++){
+		coords=[Math.random()*cnvs.width, Math.random()*cnvs.height]
+		pos.push(coords)
+		points.push(new Point({id:pos.length-1}))
+	}
 	animate()
 }
 function clickPos(e) {
-    const rect = cnvs.getBoundingClientRect()
-    const x = e.clientX - rect.left
-    const y = e.clientY - rect.top
+	const rect = cnvs.getBoundingClientRect()
+	const x = e.clientX - rect.left
+	const y = e.clientY - rect.top
 	return [x, y]
 }
 
+cnvs.addEventListener('mousemove',(e)=>{
+	const coords = clickPos(e)
+	specialPoints[0]=coords
+	//points.push(new Point({id:pos.length-1}))
+
+})
 cnvs.addEventListener('click',(e)=>{
+	console.log(1)
 	const coords = clickPos(e)
 	pos.push(coords)
 	points.push(new Point({id:pos.length-1}))
